6e4b90f1117bfd855acdaf9160f97b1d544b719b
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-05-18 14:57:45 -0700

    Added methods for adding and removing ingredients and also added totalPrice property in state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,14 @@ import React, {Component} from 'react';
 import Aux from "../../hoc/Auxillary";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+	salad: 1,
+	cheese: 0.5,
+	bacon: 2,
+	meat: 2
+};
+
 class BurgerBuilder extends Component {
 
 	// constructor(props) {
@@ -16,7 +24,34 @@ class BurgerBuilder extends Component {
 			bacon: 0,
 			cheese: 0,
 			meat: 0
-		}
+		},
+		totalPrice: 4
+	}
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const newCount = oldCount + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = newCount;
+		const oldPrice = this.state.totalPrice;
+		const priceToBeAdded = INGREDIENT_PRICES[type];
+		const newPrice = oldPrice + priceToBeAdded;
+		this.setState({ingredients: updatedIngredients, totalPrice: newPrice})
+	}
+
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const newCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = newCount;
+		const oldPrice = this.state.totalPrice;
+		const priceToBeAdded = INGREDIENT_PRICES[type];
+		const newPrice = oldPrice - priceToBeAdded;
+		this.setState({ingredients: updatedIngredients, totalPrice: newPrice})
 	}
 	//lifecycle method to implement
 	render() {
@@ -25,7 +60,9 @@ class BurgerBuilder extends Component {
 		//wrap it in hoc
 		<Aux>
 			<Burger ingredients={this.state.ingredients}/>
-			<BuildControls />
+			<BuildControls 
+				ingredientAdded = {this.addIngredientHandler}
+				ingredientRemoved = {this.removeIngredientHandler}/>
 		</Aux>
 
 		);
