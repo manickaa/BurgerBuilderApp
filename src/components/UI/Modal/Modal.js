655912ade20349ee2e80d251616e47214501d090
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-05-27 15:05:59 -0700

    Implemented backdrop component inside modal, when clicked disappears and shows when order button is clicked

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
  import React from 'react';
 
  import classes from './Modal.css';
+ import Aux from "../../../hoc/Auxillary";
+ import Backdrop from "../Backdrop/Backdrop";
 
  const modal = (props) => (
  //dynamic thing must be a js object..that is why we need double curly braces
- 	<div 
- 		className = {classes.Modal}
- 		style={{
- 			transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
- 			opacity: props.show ? '1': ''
- 		}}>
- 		{props.children}
- 	</div>
+ 	<Aux>
+ 		<Backdrop show={props.show} clicked={props.modalClosed}/>
+	 	<div 
+	 		className = {classes.Modal}
+	 		style={{
+	 			transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+	 			opacity: props.show ? '1': ''
+	 		}}>
+	 		{props.children}
+	 	</div>
+ 	</Aux>
 
 
  );
