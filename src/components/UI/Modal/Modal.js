363e17ad0bd4615b814da27e00ce664498cb525f
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-05-27 14:48:01 -0700

    Added animation to get the modal only when order now button is clicked

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,8 +3,13 @@
  import classes from './Modal.css';
 
  const modal = (props) => (
- 	
- 	<div className = {classes.Modal}>
+ //dynamic thing must be a js object..that is why we need double curly braces
+ 	<div 
+ 		className = {classes.Modal}
+ 		style={{
+ 			transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+ 			opacity: props.show ? '1': ''
+ 		}}>
  		{props.children}
  	</div>
 
