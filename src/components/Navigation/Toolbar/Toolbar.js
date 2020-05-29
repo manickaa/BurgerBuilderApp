fa23e7922cee8c3d024b65a2ac85836031efb7f2
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-05-29 16:54:12 -0700

    added class name for div wrapping up the logo in side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,12 @@ import NavigationItems from "../NavigationItems/NavigationItems";
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div> MENU </div>
-		<Logo />
-		<NavigationItems />
+		<div className={classes.Logo}>
+			<Logo height="80%"/>
+		</div>
+		<nav>
+			<NavigationItems />
+		</nav>
 	</header>
 
 );
