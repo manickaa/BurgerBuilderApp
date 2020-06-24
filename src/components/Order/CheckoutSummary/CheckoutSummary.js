f5ce4f6932a57b70e9d1b57f72353bec64b1895c
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-06-24 15:55:43 -0700

    Added our own buttons and burger builded to the checkout summary

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -5,7 +5,6 @@ import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
 	return(
-
 		<div className={classes.CheckoutSummary}>
 			<h1> We hope it tastes well! </h1>
 			<div style={{width: '100%', margin: 'auto'}}>
@@ -13,10 +12,10 @@ const checkoutSummary = (props) => {
 			</div>
 			<Button 
 				btnType="Danger"
-				clicked>CANCEL</Button>
+				clicked={props.checkoutCancelled}>CANCEL</Button>
 			<Button 
 				btnType="Success"
-				clicked>CONTINUE</Button>
+				clicked={props.checkoutContinued}>CONTINUE</Button>
 		</div>
 	);
 }
