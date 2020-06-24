5fbd012508db9b74ebc4483eade2362bd59cdd90
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-06-24 15:56:28 -0700

    Changes in props

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -5,7 +5,7 @@ import classes from './Backdrop.css';
 const backdrop = (props) => (
 	props.show ? <div 
 		className={classes.Backdrop}
-		onClick = {props.clicked}> </div> : null
+		onClick = {props.clicked}></div> : null
 );
 
 export default backdrop;
