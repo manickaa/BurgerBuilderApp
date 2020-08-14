22afde934ffc0473dde391e910ee987c8f8cdf7b
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-08-14 13:02:03 -0700

    Import actions to dispatch them as props to the components and provide data to the components from the store

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 import {connect} from 'react-redux';
-
-class Checkout extends Component {
-	
+import * as actions from '../../store/actions/index';
+class Checkout extends Component {	
 
 	checkoutCancelHandler = () => {
 		this.props.history.goBack();   //goes back to the last page
@@ -16,25 +15,31 @@ class Checkout extends Component {
 	}
 
  	render() {
-		return (
-			<div>
-				<CheckoutSummary 
-					ingredients={this.props.ings} 
-					checkoutCancelled={this.checkoutCancelHandler}
-					checkoutContinued={this.checkoutContinueHandler} />
-				<Route path={this.props.match.path + '/contact-data'} 
-					   component = {ContactData} />
-			</div>
-		);
+ 		let summary = <Redirect to="/" />
+ 		if(this.props.ings) {
+ 			const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
+ 			summary = (
+ 				<div>
+ 					{purchasedRedirect}
+	 				<CheckoutSummary 
+						ingredients={this.props.ings} 
+						checkoutCancelled={this.checkoutCancelHandler}
+						checkoutContinued={this.checkoutContinueHandler} />
+					<Route path={this.props.match.path + '/contact-data'} 
+						   component = {ContactData} />
+				</div>
+			);
+ 		}
+		return summary;
 	}
 }
 
 const mapStateToProps = state => {
 	return {
-		ings: state.ingredients,
+		ings: state.burgerbuilder.ingredients,
+		purchased: state.order.purchased
 		//price: state.totalPrice
 	};
 };
-
 
 export default connect(mapStateToProps)(Checkout);
