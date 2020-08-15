437db4658d90fdff94d349ccb6685ed182ae489b
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-08-14 17:10:44 -0700

    Get the state (isAuthenticated) from store and pass it to Toolbar and sidedrawer

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 import classes from './Layout.css';
 import Aux from '../Aux/Auxillary';
@@ -24,8 +25,12 @@ class Layout extends Component {
 		render () {
 			return(
 				<Aux>
-					<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-					<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+					<Toolbar
+						isAuth={this.props.isAuthenticated} 
+						drawerToggleClicked={this.sideDrawerToggleHandler}/>
+					<SideDrawer 
+						isAuth={this.props.isAuthenticated}
+						open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
 					<main className={classes.Content}>
 						{this.props.children}
 					</main>
@@ -35,5 +40,10 @@ class Layout extends Component {
 		}
  		
 	}
+const mapStateToProps = state => {
+	return {
+		isAuthenticated: state.auth.token !== null
+	};
+};
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
