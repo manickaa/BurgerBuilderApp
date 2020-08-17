c84a1512eace5b4779a8c9773ea0a5b7b4316e88
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-08-17 12:04:39 -0700

    Export the action creator for set auth redirect path

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder';
 export {purchaseBurger, purchaseInit, fetchOrders} from './order';
-export {auth, logout} from './Auth';
+export {auth, logout, setAuthRedirectPath} from './Auth';
