d36781c17100c277f3ed6c8f26f1b83cc08243a3
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-08-17 17:46:16 -0700

    Used the token as auth and userId to orderBy in the query params for fetching related orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -62,23 +62,23 @@ export const fetchOrdersStart = () => {
 	};
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
 	return dispatch => {
 		dispatch(fetchOrdersStart());
-		axios.get('/orders.json?auth=' + token)
-			.then(res => {
-				const fetchedOrders = [];
-				for (let key in res.data) {
-					fetchedOrders.push({
-						...res.data[key],
-						id: key
-					});
-				}
-				dispatch(fetchOrdersSuccess(fetchedOrders));
-				//console.log(this.state.orders);
-			})
-			.catch(error => {
-				dispatch(fetchOrdersFail(error));
-			});
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get( '/orders.json' + queryParams)
+            .then( res => {
+                const fetchedOrders = [];
+                for ( let key in res.data ) {
+                    fetchedOrders.push( {
+                        ...res.data[key],
+                        id: key
+                    } );
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            } )
+            .catch( err => {
+                dispatch(fetchOrdersFail(err));
+            } );
 	};
 };
